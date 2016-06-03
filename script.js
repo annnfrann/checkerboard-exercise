72a13a9fd62445bfaffae7145097738debdc8b09
Anna Lewis <annalewis@Annas-MacBook-Air.local>
2016-06-03 15:14:08 -0600

    finished random colors checkerboard

--- script.js
+++ script.js
@@ -1,48 +1,11 @@
-// // Your JS goes here
-// var newDiv = document.createElement("div");
-// newDiv.style.backgroundColor = "black";
-// newDiv.style.width = "11.1%";
-// newDiv.style.float = "left";
-// newDiv.style.paddingBottom = "11.1%";
-// document.getElementsByTagName("body")[0].appendChild(newDiv);
-//
-// var newDiv = document.createElement("div");
-// newDiv.style.backgroundColor = "red";
-// newDiv.style.width = "11.1%";
-// newDiv.style.float = "left";
-// newDiv.style.paddingBottom = "11.1%";
-// document.getElementsByTagName("body")[0].appendChild(newDiv);
-//
-// var newDiv = document.createElement("div");
-// newDiv.style.backgroundColor = "black";
-// newDiv.style.width = "11.1%";
-// newDiv.style.float = "left";
-// newDiv.style.paddingBottom = "11.1%";
-// document.getElementsByTagName("body")[0].appendChild(newDiv);
-
-// THere's no such place as North Carolina
-
-for (var i = 0; i < 41; i++) {
-  if (i === 40) {
+for (var i = 0; i < 81; i++) {
+  var newColor = Math.random()* 1000000;
+  newColor = newColor.toFixed(0);
+  console.log(newColor);
     var newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = "black";
+    newDiv.style.backgroundColor = "#" + newColor;
     newDiv.style.width = "11.1%";
     newDiv.style.float = "left";
     newDiv.style.paddingBottom = "11.1%";
     document.getElementsByTagName("body")[0].appendChild(newDiv);
-  } else {
-    var newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = "black";
-    newDiv.style.width = "11.1%";
-    newDiv.style.float = "left";
-    newDiv.style.paddingBottom = "11.1%";
-    document.getElementsByTagName("body")[0].appendChild(newDiv);
-
-    var newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = "red";
-    newDiv.style.width = "11.1%";
-    newDiv.style.float = "left";
-    newDiv.style.paddingBottom = "11.1%";
-    document.getElementsByTagName("body")[0].appendChild(newDiv);
-  }
 }
