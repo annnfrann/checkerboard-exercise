f3ce116ae7eeeb132683fef92afab0e531b92777
Anna Lewis <annalewis@Annas-MacBook-Air.local>
2016-06-03 16:09:34 -0600

    flashing checkerboard completed

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-//make this "refresh every 2 seconds without refreshing the page"
+var body = document.body;
+
+var holder = document.createElement("div");
+holder.style.height = "100vh";
+holder.style.width = "100vw";
+body.appendChild(holder);
+body.style.margin = "0px";
 
 
+function flash(){
+holder.innerHTML = ""
 for (var i = 0; i < 81; i++) {
-  var newColor = Math.random()* 1000000;
-  newColor = newColor.toFixed(0);
-  console.log(newColor);
+  var newColor = Math.floor((Math.random() * 899999) + 99999)
     var newDiv = document.createElement("div");
     newDiv.style.backgroundColor = "#" + newColor;
     newDiv.style.width = "11.1%";
     newDiv.style.float = "left";
     newDiv.style.paddingBottom = "11.1%";
-    document.getElementsByTagName("body")[0].appendChild(newDiv);
+    holder.appendChild(newDiv);
+  }
+  setTimeout(flash, 2000)
 }
+
+flash()
