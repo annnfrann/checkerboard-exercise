c2a3f5614d0e5e74292d7c37a777b6209f538069
Anna Lewis <annalewis@Annas-MacBook-Air.local>
2016-06-03 16:52:44 -0600

    finished the gradient

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
+// for (var i = 0; i < 81; i++) {
+//   var newColor = Math.floor((Math.random() * 899999) + 99999)
+//     var newDiv = document.createElement("div");
+//     newDiv.style.backgroundColor = "#" + newColor;
+//     newDiv.style.width = "11.1%";
+//     newDiv.style.float = "left";
+//     newDiv.style.paddingBottom = "11.1%";
+//     document.getElementsByTagName("body")[0].appendChild(newDiv);
+// }
+
+
+// if (i%2 === 0) {
+//   colorNumber1 + 3
+// } else {
+//   colorNumber2 + 3
+// }
+var colorNumber1 = 299010;
+var colorNumber2 = 293010;
+
 for (var i = 0; i < 81; i++) {
-  var newColor = Math.floor((Math.random() * 899999) + 99999)
+  if (i%2===0) {
+    colorNumber1 += 3;
+    console.log("first:", + colorNumber1);
     var newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = "#" + newColor;
-    newDiv.style.width = "11.1%";
+    newDiv.style.backgroundColor = "#" + colorNumber1;    newDiv.style.width = "11.1%";
     newDiv.style.float = "left";
     newDiv.style.paddingBottom = "11.1%";
     document.getElementsByTagName("body")[0].appendChild(newDiv);
+  } else {
+    colorNumber2 += 100;
+    console.log("second:", + colorNumber2);
+    var newDiv = document.createElement("div");
+    newDiv.style.backgroundColor = "#" + colorNumber2;    newDiv.style.width = "11.1%";
+    newDiv.style.float = "left";
+    newDiv.style.paddingBottom = "11.1%";
+    document.getElementsByTagName("body")[0].appendChild(newDiv);
+  }
 }
