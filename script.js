b0b2d72c2e42c9a6dcad477a46af5edb8a780460
Anna Lewis <annalewis@Annas-MacBook-Air.local>
2016-06-04 12:41:49 -0600

    pushing to the correct branch this time

--- script.js
+++ script.js
@@ -14,12 +14,12 @@
 // } else {
 //   colorNumber2 + 3
 // }
-var colorNumber1 = 299010;
-var colorNumber2 = 293010;
+var colorNumber1 = 299000;
+var colorNumber2 = 293000;
 
 for (var i = 0; i < 81; i++) {
   if (i%2===0) {
-    colorNumber1 += 3;
+    colorNumber1 += 100;
     console.log("first:", + colorNumber1);
     var newDiv = document.createElement("div");
     newDiv.style.backgroundColor = "#" + colorNumber1;    newDiv.style.width = "11.1%";
